1eef4dddeef683630c845bab0cbee05ad11fd472
Volodymyr Dymka <dymkavova@gmail.com>
2025-07-18 18:38:28 +0200

    Migrate to `picamera2` library and refactor server setup logic.

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
-
+# !/usr/bin/env python
 
 import io
-import picamera
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FileOutput
 
 from os import curdir, sep
 from string import Template
@@ -30,20 +31,18 @@ FRAMERATE = 25
 HTTP_PORT = 8082
 WS_PORT = 8084
 
-
 VFLIP = True
 HFLIP = False
 
-###########################################
 
+###########################################
 
 class StreamingHttpHandler(BaseHTTPRequestHandler):
     def do_HEAD(self):
         self.do_GET()
 
-
     def do_GET(self):
-        #Serve index.html
+        # Serve index.html
         if self.path == '/':
             self.send_response(301)
             self.send_header('Location', '/index.html')
@@ -54,27 +53,25 @@ class StreamingHttpHandler(BaseHTTPRequestHandler):
             tpl = Template(self.server.index_template)
             content = tpl.safe_substitute(dict(
                 ADDRESS='%s:%d' % (self.request.getsockname()[0], WS_PORT)
-                ))
-        #Serve js
+            ))
+        # Serve js
         elif self.path.startswith('/js/'):
             f = open(curdir + sep + self.path)
-            
             self.send_response(200)
-            self.send_header('Content-type',    'application/javascript')
+            self.send_header('Content-type', 'application/javascript')
             self.end_headers()
-            self.wfile.write(bytes(f.read(),encoding='utf-8'))
+            self.wfile.write(bytes(f.read(), encoding='utf-8'))
             f.close()
             return
-        #Serve css
+        # Serve css
         elif self.path.startswith('/css/'):
             f = open(curdir + sep + self.path)
             self.send_response(200)
-            self.send_header('Content-type',    'text/css')
+            self.send_header('Content-type', 'text/css')
             self.end_headers()
-            self.wfile.write(bytes(f.read(),encoding='utf-8'))
+            self.wfile.write(bytes(f.read(), encoding='utf-8'))
             f.close()
             return
-            
         else:
             self.send_error(404, 'File not found')
             return
@@ -89,12 +86,10 @@ class StreamingHttpHandler(BaseHTTPRequestHandler):
             self.wfile.write(content)
 
 
-
 class StreamingHttpServer(HTTPServer):
     def __init__(self):
-       
         super(StreamingHttpServer, self).__init__(
-                    ('', HTTP_PORT), StreamingHttpHandler)
+            ('', HTTP_PORT), StreamingHttpHandler)
         with io.open('index.html', 'r') as f:
             self.index_template = f.read()
 
@@ -102,63 +97,69 @@ class StreamingHttpServer(HTTPServer):
 class StreamingWebSocket(WebSocket):
     def opened(self):
         print("New client connected")
-        # you can override various WebSocket class methods
-        # to do more stuff with WebSockets other than streaming
-        
 
 
 def main():
-
-    #Camera and the configuration
+    # Camera initialization and configuration
     print('Initializing camera')
-    camera = picamera.PiCamera()
-    camera.framerate = FRAMERATE
-    camera.resolution = (WIDTH, HEIGHT)
-    camera.vflip = VFLIP # flips image rightside up, as needed
-    camera.hflip = HFLIP # flips image left-right, as needed
-    sleep(1) # camera warm-up time
+    camera = Picamera2()
+    video_config = camera.create_video_configuration(
+        main={"size": (WIDTH, HEIGHT)},
+        controls={"FrameRate": FRAMERATE}
+    )
+    camera.configure(video_config)
 
-    #Custom output for h264 stream
-    output = StreamingOutput()    
+    # Set up the H264 encoder
+    encoder = H264Encoder()
+    output = StreamingOutput()
 
-    #Websocket
+    if VFLIP:
+        camera.set_controls({"Rotate": 180})
+    if HFLIP:
+        camera.set_controls({"HFlip": True})
+
+    camera.start()
+    sleep(1)  # Camera warm-up time
+
+    # Websocket setup
     print('Initializing websockets server on port %d' % WS_PORT)
     WebSocketWSGIHandler.http_version = '1.1'
     websocket_server = make_server(
-            '', WS_PORT,
-            server_class=WSGIServer,
-            handler_class=WebSocketWSGIRequestHandler,
-            app=WebSocketWSGIApplication(handler_cls=StreamingWebSocket))
+        '', WS_PORT,
+        server_class=WSGIServer,
+        handler_class=WebSocketWSGIRequestHandler,
+        app=WebSocketWSGIApplication(handler_cls=StreamingWebSocket))
 
     websocket_server.initialize_websockets_manager()
     websocket_thread = Thread(target=websocket_server.serve_forever)
 
-    #Http
+    # Http setup
     print('Initializing HTTP server on port %d' % HTTP_PORT)
     http_server = StreamingHttpServer()
     http_thread = Thread(target=http_server.serve_forever)
-   
-    #Broadcast
+
+    # Broadcast setup
     print('Initializing broadcast thread')
     broadcast_thread = BroadcastThread(camera, output, websocket_server)
 
-
-    # Start the threads here   
+    # Start all threads
     try:
         print('Starting websockets thread')
         websocket_thread.start()
         print('Starting HTTP server thread')
         http_thread.start()
-        print('Starting recording and broadcastasting thread')
+        print('Starting recording and broadcasting thread')
+        camera.start_recording(encoder, FileOutput(output))
         broadcast_thread.start()
+
         while True:
-                pass
+            sleep(1)
     except KeyboardInterrupt:
         pass
     finally:
-
         print('Stopping recording')
         camera.stop_recording()
+        camera.stop()
         print('Waiting for broadcast thread to finish')
         broadcast_thread.join()
         print('Shutting down HTTP server')
@@ -169,7 +170,6 @@ def main():
         http_thread.join()
         print('Waiting for websockets thread to finish')
         websocket_thread.join()
-    
 
 
 if __name__ == '__main__':
